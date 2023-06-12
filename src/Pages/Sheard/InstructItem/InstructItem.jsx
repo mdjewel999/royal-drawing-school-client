6e14d22c85574c3691266e771b5a636e38e1fb9f
mdjewel999 <mdjewel878761@gmail.com>
2023-06-12 23:50:14 +0600

    Home Page Animation code update

--- src/Pages/Sheard/InstructItem/InstructItem.jsx
+++ src/Pages/Sheard/InstructItem/InstructItem.jsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 
+const InstructItem = ({ item }) => {
+  const { name, image, email } = item;
+  const [isHovered, setIsHovered] = useState(false);
 
-const InstructItem = ({item}) => {
-    const {name, image, email}= item;
-    return (
-        <div>
-            <img className="w-[400px] h-[250px]" src={image} alt="" />
-            <h1>Name: {name}</h1>
-            <p>Email: {email}</p>
-        </div>
-    );
+  const animationProps = useSpring({
+    transform: isHovered ? 'scale(1.1)' : 'scale(1)',
+    config: { tension: 300, friction: 10 },
+  });
+
+  return (
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <animated.div style={animationProps}>
+        <img className="w-[400px] h-[250px]" src={image} alt="" />
+        <h1>Name: {name}</h1>
+        <p>Email: {email}</p>
+      </animated.div>
+    </div>
+  );
 };
 
 export default InstructItem;
--- src/Pages/Sheard/StudentesClass/StudentesClass.jsx
+++ src/Pages/Sheard/StudentesClass/StudentesClass.jsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 
+const StudentesClass = ({ item }) => {
+  const { name, instructorName, price, availableSeats, image } = item;
+  const [isHovered, setIsHovered] = useState(false);
 
-const StudentesClass = ({item}) => {
-    const {name, instructorName, price, availableSeats, image}= item;
-    // console.log(item);
-    return (
-        <div>
-            <img className="w-[400px] h-[250px]" src={image} alt="" />
-            <h1>Name: {name}</h1>
-            <p>instructor Name: {instructorName}</p>
-            <p>price: $ {price}</p>
-            <p>available Seats: {availableSeats}</p>
-        </div>
-    );
+  const animationProps = useSpring({
+    transform: isHovered ? 'scale(1.1)' : 'scale(1)',
+    config: { tension: 300, friction: 10 },
+  });
+
+  return (
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <animated.div style={animationProps}>
+        <img className="w-[400px] h-[250px]" src={image} alt="" />
+        <h1>Name: {name}</h1>
+        <p>Instructor Name: {instructorName}</p>
+        <p>Price: $ {price}</p>
+        <p>Available Seats: {availableSeats}</p>
+      </animated.div>
+    </div>
+  );
 };
 
 export default StudentesClass;
